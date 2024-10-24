1dbab3741e4b5786dddac64fc48104724d959140
José Luis Soto <54287316+jlso1o@users.noreply.github.com>
2024-10-23 19:38:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import os
 import subprocess
-
-# Instala matplotlib en tiempo de ejecución si no está instalado
-try:
-    import matplotlib
-except ImportError:
-    subprocess.check_call([os.sys.executable, '-m', 'pip', 'install', 'matplotlib'])
-
+import matplotlib
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
